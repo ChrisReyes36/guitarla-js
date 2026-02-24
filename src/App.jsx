4d05bf9e8c433ebe42120ee2b473e1e27ad34f7c
Christopher Muñoz <christopherreyes456@gmail.com>
2026-02-23 23:19:42 -0600

    feature(proyecto): proyecto finalizado

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import Footer from "./components/Footer";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { useCart } from "./hooks/useCart";
+
+function App() {
+  const {
+    cart,
+    cartTotal,
+    data,
+    isEmpty,
+    addToCart,
+    clearCart,
+    decreaseQuantity,
+    increaseQuantity,
+    removeFromCart,
+  } = useCart();
+
+  return (
+    <>
+      <Header
+        cart={cart}
+        cartTotal={cartTotal}
+        isEmpty={isEmpty}
+        removeFromCart={removeFromCart}
+        decreaseQuantity={decreaseQuantity}
+        increaseQuantity={increaseQuantity}
+        clearCart={clearCart}
+      />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
